510894ac315a7621613bc1a8b553017b8f66d55c
bennyhi <fer17nanno.bennyhi@gmail.com>
2023-02-05 12:50:23 -0400

    Endpoint para el  Administrador

--- src/routes/muestreo.routes.js
+++ src/routes/muestreo.routes.js
@@ -7,6 +7,17 @@ const router = Router()
 
 router.get('/inicio', muestreoController.getInicio)
 router.get('/sobre-munay', muestreoController.getSobreMunay)
+router.get('/muestreoUser', muestreoController.getMuestreoUser)
+router.get('/muestreoEmprendimiento', muestreoController.getMuestreoEmprendimiento)
+router.get('/muestreoMentor', muestreoController.getMuestreoEmprendimiento)
+router.get('/muestreoKumpita', muestreoController.getMuestreoKumpita)
+router.get('/muestreoPublicaciones', muestreoController.getMuestreoPublicaciones)
+router.get('/muestreoTestimonio', muestreoController.getMuestreoTestimonio)
+
+
+
+
+
 
 
 export default router;
