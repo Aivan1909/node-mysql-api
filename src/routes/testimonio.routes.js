3e4b7213bd443a2fc14166ad76ba230585ccca95
Ivan Terrazas <aivatepaz@gmail.com>
2023-02-04 20:46:20 -0400

    correccion de router testimonio en el cargado de imagenes.

--- src/routes/testimonio.routes.js
+++ src/routes/testimonio.routes.js
@@ -1,14 +1,15 @@
 import { Router } from 'express'
 import { methods as testimonioController } from './../controllers/testimonio.controller'
 const authenticate = require('./../middleware/authenticate')
-const { uploadImg } = require('./../middleware/upload');
+const { isImage } = require('./../middleware/upload');
+import multer from 'multer';
 
 const router = Router()
 
-router.post('/', uploadImg.single('imagen'), testimonioController.addTestimonios) //authenticate
+router.post('/', authenticate, multer().single('imagen'), isImage, testimonioController.addTestimonios) //authenticate
 router.get('/', testimonioController.getTestimonios)
 router.get('/:id', testimonioController.getTestimonio)
-router.put('/:id', authenticate, uploadImg.single('imagen'), testimonioController.updateTestimonio)
+router.put('/:id', authenticate, multer().single('imagen'), isImage, testimonioController.updateTestimonio)
 router.delete('/:id', authenticate, testimonioController.deleteTestimonio)
 
 export default router;
