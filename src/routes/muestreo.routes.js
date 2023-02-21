71513439e3d89dcbd7c56ad7aee882a7d0e7bb64
bennyhi <fer17nanno.bennyhi@gmail.com>
2023-02-21 18:17:58 -0400

    correccion de nombre ruta

--- src/routes/muestreo.routes.js
+++ src/routes/muestreo.routes.js
@@ -27,7 +27,7 @@ router.get('/muestreoColaborador', muestreoController.getMuestreoColaboradores)
 router.get('/muestreoAlianza', muestreoController.getMuestreoAlianzas)
 router.get('/muestreoModalEmprendimiento', muestreoController.getModalEmprendimientos)
 router.get('/muestreoDetalleEmprendimiento/:id', muestreoController.getDetalleEmprendimiento)
-router.get('/muestreoMentores',muestreoController.getNuestroMentores)
+router.get('/muestreoMentores',muestreoController.getNuestrosMentores)
 
 
 
