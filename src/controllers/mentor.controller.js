86a1604c87d59eb9b4d7f7560e0e4eb996c82d04
bennyhi <fer17nanno.bennyhi@gmail.com>
2023-02-25 23:18:54 -0400

    Add Mentor Funcional

--- src/controllers/mentor.controller.js
+++ src/controllers/mentor.controller.js
@@ -4,9 +4,12 @@ import { SaveOneFile, deleteOneFile, getOneFile, updateOneFile } from '../middle
 const PUBLIC_URL = process.env.PUBLIC_URL;
 
 const _TABLA = 'tmunay_mentores';
-const _TABLA1 = 'horario_mentor';
+const _TABLA1 = 'horario_mentoria'
+//const _TABLA1 = 'horario_mentor';
 const _TABLA2 = 'area_mentor';
 const _TABLA3 = 'especialidad_mentor';
+const _TABLA4 = 'tmunay_horarios';
+const _TABLA5 = 'users';
 
 
 
@@ -15,9 +18,6 @@ const addMentores = async (req, res) => {
     //Horario
     const bkHorario = req.body.horario
     delete req.body.horario
-    //AreaMentor
-    const bkArea = req.body.area
-    delete req.body.area
 
     const bkEspecialidad = req.body.especialidad
     delete req.body.especialidad
@@ -28,26 +28,8 @@ const addMentores = async (req, res) => {
     const result = await connection.query(`INSERT INTO ${_TABLA} SET ?`, mentor);
 
     const { insertId} =  await result;
-    //Insertando  el id Relacional Horario
-    await bkHorario.forEach (element => {
-      const  idExternaHorario  = {
-        mentores_id: insertId,
-        horarios_id: element
-      } ;
-      //console.log("IDS->externos ->" , idExternaHorario)
-      connection.query(`INSERT INTO ${_TABLA1} SET ?`, idExternaHorario);
-    });
-      //Insertando  el id Relacional Horario
-    await bkArea.forEach (element => {
-      const  idExternaArea  = {
-        mentor_id: insertId,
-        area_id: element
-      } ;
-      //console.log("IDS->externos ->" , idExternaHorario)
-      connection.query(`INSERT INTO ${_TABLA2} SET ?`, idExternaArea);
-    });
-
-    //Insertando  el id Relacional Horario
+ 
+    //Insertando  el id  especialidad
     await bkEspecialidad.forEach (element => {
     const  idExternaEspecialidad  = {
         mentor_id: insertId,
@@ -56,6 +38,21 @@ const addMentores = async (req, res) => {
         //console.log("IDS->externos ->" , idExternaHorario)
         connection.query(`INSERT INTO ${_TABLA3} SET ?`, idExternaEspecialidad);
      });
+
+    //Insertando  los horarios del mentor
+    await bkHorario.forEach (element => {
+      const  idExternaHorario  = {
+        mentores_id: insertId,
+        dia: element.dia,
+        hora1: element.hora1,
+        hora2: element.hora2
+        //horarios_id: element
+      } ;
+      idExternaHorario.fechaCreacion = require('moment')().format('YYYY-MM-DD HH:mm:ss');
+      idExternaHorario.estado =1;
+      //console.log("IDS->externos ->" , idExternaHorario)
+      connection.query(`INSERT INTO ${_TABLA4} SET ?`, idExternaHorario);
+    });
 
     res.json({ body: result });
   } catch (error) {
@@ -118,10 +115,81 @@ const deleteMentor = async (req, res) => {
   }
 };
 
+
+const getListaMentorEsp = async (req, res) => {
+  console.log('holaaaa->>>>>>')
+  try {
+    const connection = await getConnection();
+    let sql = `SELECT  A.id, concat(D.nombre ,' ', D.apellidos) as nombres , C.duracion, A.online
+               FROM ${_TABLA} A
+               INNER JOIN ${_TABLA1} B ON A.id = B.mentores_id
+               INNER JOIN ${_TABLA4} C ON B.horarios_id = C.id
+               INNER JOIN ${_TABLA5} D ON A.user_id = D.id
+               WHERE A.tipo = 'E'`
+    const result = await connection.query(sql);
+
+
+    res.json({ body: result });
+  } catch (error) {
+    res.status(500);
+    res.json(error.message);
+  }
+};
+
+
+
+const getListaMentorD = async (req, res) => {
+  try {
+    const connection = await getConnection();
+    let sql = `SELECT  A.id, concat(D.nombre ,' ', D.apellidos) as nombres , C.duracion, A.online
+               FROM ${_TABLA} A
+               INNER JOIN ${_TABLA1} B ON A.id = B.mentores_id
+               INNER JOIN ${_TABLA4} C ON B.horarios_id = C.id
+               INNER JOIN ${_TABLA5} D ON A.user_id = D.id
+               WHERE A.tipo = 'D'`;
+    const result = await connection.query(sql);
+    res.json({ body: result });
+  } catch (error) {
+    res.status(500);
+    res.json(error.message);
+  }
+};
+
+/* const getMentorFechaHorario= async (req, res) => {
+  try {
+    const { fecha } = req.params;
+    const connection = await getConnection();
+    let sql1 = `SELECT  A.user_id, CONCAT(B.nombre , ' ', B.apellidos) AS nombres
+                FROM ${_TABLA} A, ${_TABLA5} B
+                WHERE A.user_id = B.id`
+    const result = await connection.query(sql1);
+ 
+    let resulF;
+    result.forEach(element => {
+       const { user_id, nombres  } = element;
+       let sql2 = `SELECT D.fecha, D.hora1, D.hora2
+                   FROM mentoria_mentor A, tmunay_mentorias B, horario_mentoria C,  tmunay_horarios D 
+                   WHERE A.mentorias_id = B.id and  B.id = C.mentorias_id and  C.horarios_id = D.id
+                   and A.mentores_id = 1 and DATE_FORMAT(D.fecha, '%Y-%m-%d') = '2023-02-21'`
+    });
+
+    res.json({ body: result });
+  } catch (error) {
+    res.status(500);
+    res.json(error.message);
+  }
+}; */
+
+
+
+
+
 export const methods = {
   addMentores,
   getMentores,
   getMentor,
+  getListaMentorEsp,
+  getListaMentorD,
   updateMentor,
-  deleteMentor,
+  deleteMentor
 };
