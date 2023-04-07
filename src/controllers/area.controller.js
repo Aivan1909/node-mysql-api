04759266459c2fa3cc54d83b08bfa71c9ef302e7
Ivan Terrazas <aivatepaz@gmail.com>
2023-04-06 20:17:05 -0400

    Correccion de obtencion de areas, adicion de especialidades por area

--- src/controllers/area.controller.js
+++ src/controllers/area.controller.js
@@ -1,10 +1,4 @@
 import { getConnection } from '../database/database';
-import { SaveOneFile, deleteOneFile, getOneFile, updateOneFile } from '../middleware/upload';
-
-const PUBLIC_URL = process.env.PUBLIC_URL;
-
-const _TABLA = 'tmunay_areas';
-
 
 const addAreas = async (req, res) => {
   try {
@@ -12,8 +6,10 @@ const addAreas = async (req, res) => {
     const area = req.body;
     area.fechaCreacion = require('moment')().format('YYYY-MM-DD HH:mm:ss');
     area.estado = 1;
+
     const connection = await getConnection();
-    const result = await connection.query(`INSERT INTO ${_TABLA} SET ?`, area);
+    const result = await connection.query(`INSERT INTO tmunay_areas SET ?`, area);
+
     res.json({ body: result });
   } catch (error) {
     res.status(500);
@@ -24,11 +20,18 @@ const addAreas = async (req, res) => {
 const getAreas = async (req, res) => {
   try {
     const connection = await getConnection();
-    const result = await connection.query(`SELECT * FROM ${_TABLA} where estado = '1'`);
-   // const foundareasWithImages = [...result].map((item) => {
-   // return { ...item, file: getOneFile(item.imagen) };});
-    res.json({ body: result });
+    const reAreas = await connection.query(`SELECT * FROM tmunay_areas where estado = 1`);
+
+    for (const area of reAreas) {
+      const reEspecialidades = await connection.query(`SELECT * 
+        FROM tmunay_especialidad WHERE estado=1 and areas_id = ?`, area.id)
+
+      area.especialidades = reEspecialidades
+    }
+
+    res.json({ body: reAreas });
   } catch (error) {
+    console.log(error)
     res.status(500);
     res.json(error.message);
   }
@@ -38,10 +41,16 @@ const getArea = async (req, res) => {
   try {
     const { id } = req.params;
     const connection = await getConnection();
-    const result = await connection.query(`SELECT * FROM ${_TABLA} WHERE id=? and estado = '1'`, id);
-    if (!result.length > 0) return res.status(404);
+    const reAreas = await connection.query(`SELECT * FROM tmunay_areas WHERE id=? and estado = 1`, id);
+
+    const result = reAreas[0]
+    const reEspecialidades = await connection.query(`SELECT * 
+        FROM tmunay_especialidad WHERE estado=1 and areas_id = ?`, result.id)
+
+    result.especialidades = reEspecialidades
+
     //const image = getOneFile(result[0].imagen);
-    res.json({ body: { ...result[0] } });
+    res.json({ body: result });
   } catch (error) {
     res.status(500);
     res.json(error.message);
@@ -49,27 +58,27 @@ const getArea = async (req, res) => {
 };
 
 const updateArea = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { nombre, descripcionArea  } = req.body;
-        if (nombre === undefined) return res.status(400).json({ message: 'Bad Request' });
-        const areas = {nombre, descripcionArea };
-        areas.fechaModificacion = require('moment')().format('YYYY-MM-DD HH:mm:ss');
-        const connection = await getConnection();
-        await connection.query(`UPDATE ${_TABLA} SET ? WHERE id=?`, [areas, id]);
-        const foundareas = await connection.query(`SELECT * FROM ${_TABLA} WHERE id=?`, id);
-        res.json({ body: foundareas[0] });
-    } catch (error) {
-        res.status(500);
-        res.json(error.message);
-    }
+  try {
+    const { id } = req.params;
+    const { nombre, descripcionArea } = req.body;
+    if (nombre === undefined) return res.status(400).json({ message: 'Bad Request' });
+    const areas = { nombre, descripcionArea };
+    areas.fechaModificacion = require('moment')().format('YYYY-MM-DD HH:mm:ss');
+    const connection = await getConnection();
+    await connection.query(`UPDATE tmunay_areas SET ? WHERE id=?`, [areas, id]);
+    const foundareas = await connection.query(`SELECT * FROM tmunay_areas WHERE id=?`, id);
+    res.json({ body: foundareas[0] });
+  } catch (error) {
+    res.status(500);
+    res.json(error.message);
+  }
 };
 
 const deleteArea = async (req, res) => {
   try {
     const { id } = req.params;
     const connection = await getConnection();
-    const result = await connection.query(`DELETE FROM ${_TABLA} WHERE id=?`, id);
+    const result = await connection.query(`DELETE FROM tmunay_areas WHERE id=?`, id);
     res.json({ body: result });
   } catch (error) {
     res.status(500);
